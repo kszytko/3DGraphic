81f02a0a50d184cc9380c5de17c28347e7157ce0
KS <mail@kszytko.pl>
2022-01-29 15:11:20 +0100

    Working update

--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@ private:
 	Matrix4 matProj, matResize, matRotZ, matRotX;
 	std::vector<Triangle> triangles;
 
-	float screenRatio;
-	float fTheta;
 
+	float fTheta = 0.0f;
+	int count = 0;
 
 
 
@@ -61,33 +61,32 @@ public:
 
 		};
 
-		screenRatio = (float)ScreenHeight() / (float)ScreenWidth();
 
 		matProj.m[0][0] = 0.5f * (float)ScreenHeight();
 		matProj.m[1][0] = 0;
 		matProj.m[2][0] = 0;
-		matProj.m[3][0] = 0;
+		matProj.m[3][0] = 0.5f * (float)ScreenWidth();
 
 		matProj.m[0][1] = 0;
 		matProj.m[1][1] = 0.5f * (float)ScreenHeight();
 		matProj.m[2][1] = 0;
-		matProj.m[3][1] = 0;
+		matProj.m[3][1] = 0.5f * (float)ScreenHeight();
 
 		matProj.m[0][2] = 0;
 		matProj.m[1][2] = 0;
 		matProj.m[2][2] = 1;
 		matProj.m[3][2] = 0;
 
-		matProj.m[0][3] = 0.5f * (float)ScreenWidth();
-		matProj.m[1][3] = 0.5f * (float)ScreenHeight();
+		matProj.m[0][3] = 0;
+		matProj.m[1][3] = 0;
 		matProj.m[2][3] = 0;
 		matProj.m[3][3] = 1;
 
-		matResize.m[0][0] = 0.5f;
+		matResize.m[0][0] = 1;
 		matResize.m[1][0] = 0;
 		matResize.m[2][0] = 0;
 		matResize.m[0][1] = 0;
-		matResize.m[1][1] = 0.5f;
+		matResize.m[1][1] = 1;
 		matResize.m[2][1] = 0;
 		matResize.m[0][2] = 0;
 		matResize.m[1][2] = 0;
@@ -109,16 +108,16 @@ public:
 		matRotZ.m[0][1] = sinf(fTheta);
 		matRotZ.m[1][0] = -sinf(fTheta);
 		matRotZ.m[1][1] = cosf(fTheta);
-		matRotZ.m[2][2] = 1;
-		matRotZ.m[3][3] = 1;
+		matRotZ.m[2][2] = 1.0f;
+		matRotZ.m[3][3] = 1.0f;
 
 		// Rotation X
-		matRotX.m[0][0] = 1;
+		matRotX.m[0][0] = 1.0f;
 		matRotX.m[1][1] = cosf(fTheta * 0.5f);
 		matRotX.m[1][2] = sinf(fTheta * 0.5f);
 		matRotX.m[2][1] = -sinf(fTheta * 0.5f);
 		matRotX.m[2][2] = cosf(fTheta * 0.5f);
-		matRotX.m[3][3] = 1;
+		matRotX.m[3][3] = 1.0f;
 
 		for (auto& tris : meshCube.tris) {
 			Triangle triProjected, triRotatedX, triRotatedXZ, triResized;
@@ -127,16 +126,16 @@ public:
 			//Projection
 		
 
-			triRotatedX =  tris.multiplyByMatrix(matRotZ);
-			//triRotatedXZ = triRotatedX.multiplyByMatrix(matRotZ);
-			triRotatedXZ = triRotatedX;
+			triRotatedX =  tris.multiplyByMatrix(matRotX);
+		    triRotatedXZ = triRotatedX.multiplyByMatrix(matRotZ);
+			//triRotatedXZ = tris;
 
-			triResized = triRotatedXZ.multiplyByMatrix(matResize);
-			triResized.calculateCrossProduct();
-			triResized.calculateDotProduct(Vec3::m_North);
+			//triResized = triRotatedXZ.multiplyByMatrix(matResize);
+			//triResized.calculateCrossProduct();
+			//triResized.calculateDotProduct(Vec3::m_North);
 
 
-			triProjected = triResized.multiplyByMatrix(matProj);
+			triProjected = triRotatedXZ.multiplyByMatrix(matProj);
 
 						
 
@@ -173,24 +172,22 @@ public:
 		//	return t1.p[0].z + t1.p[1].z + t1.p[2].z > t2.p[0].z + t2.p[1].z + t2.p[2].z;
 		//	});
 
-		int count = 0;
+
 		for (auto& tris : triangles) {
 
 			//if (tris.dot = 0) {
 			//FillTriangle(tris.p[0].x, tris.p[0].y, tris.p[1].x, tris.p[1].y, tris.p[2].x, tris.p[2].y, olc::WHITE);
 			DrawTriangle(tris.p[0].x, tris.p[0].y, tris.p[1].x, tris.p[1].y, tris.p[2].x, tris.p[2].y, olc::WHITE);
-			DrawLine(tris.p[0].x, tris.p[0].y, 
-				tris.cross.x , 
-				tris.cross.y , 
-				olc::YELLOW);
+			//DrawLine(tris.p[0].x, tris.p[0].y, tris.cross.x , tris.cross.y ,olc::YELLOW);
 				
 				
 			//}
 
 			
 
-			DrawString(0, count++ * 10, std::to_string(tris.dot), olc::WHITE, 1);
+			//DrawString(0, count++ * 10, std::to_string(tris.dot), olc::WHITE, 1);
 		}
+		DrawString(0, 0, std::to_string(count++), olc::WHITE, 1);
 
 		return true;
 	}
