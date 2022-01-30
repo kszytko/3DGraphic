b12279b691c6e64f199d233b4e02897699c0f5a9
KS <mail@kszytko.pl>
2022-01-30 22:26:31 +0100

    Update missing symbols

--- Cube.h
+++ Cube.h
@@ -1,3 +1,6 @@
+#pragma once
+
+#include "Mesh.h"
 
 class Cube : public Mesh {
 	Cube (float size = 1.0f)
@@ -13,15 +16,15 @@ class Cube : public Mesh {
 			 side, -side,  side, // 5
 			-side,  side,  side, // 6
 			 side,  side,  side  // 7
-		}
+		};
 
-		indicates = { 
+		indicates = {
 				0,2,1, 2,3,1,
 				1,3,5, 3,7,5,
 				2,6,3, 3,6,7,
 				4,5,7, 4,7,6,
 				0,4,2, 2,4,6,
 				0,1,4, 1,5,4
-		}
+		};
 	}
 };
